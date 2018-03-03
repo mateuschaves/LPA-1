14363f0498b1be197883d24121b25671525a3e1c
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-03-03 17:21:39 -0300

    Mudança na declaração de algumas variáveis

--- Exercícios de monitoria 01/b.c
+++ Exercícios de monitoria 01/b.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 /*
- *Crie um programa que exiba a mÈdia de pedro, Utilize vari·vel do tipo Float.
- *FÛrmula - mÈdia = (Nota 1 + Nota 2 + Nota 3) / 3
+ *  Crie um programa que exiba a m√©dia de pedro, Utilize vari√°vel do tipo Float.
+ *
+ *  F√≥rmula - m√©dia = (Nota 1 + Nota 2 + Nota 3) / 3
+ *
+ *  Notas = 9.0, 3.0, 5.0
  */
 
  int main(void) {
 
     float notas[] = {9.0, 3.0, 5.0};
-    float soma;
-    float media;
-    int i;
+    float soma, media;
+    int i, tamanho;
 
     for(i = 0; i < sizeof(notas)/sizeof(int); i++){
       soma += notas[i];
     }
 
-    int tamanho = sizeof(notas)/sizeof(int);
+    tamanho = sizeof(notas)/sizeof(int);
 
     media = soma/tamanho;
 
-    printf("MÈdia: %f", media);
+    printf("M√©dia: %f", media);
 
     return 0;
  }
