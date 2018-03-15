b22fa69fe705efa566f07e8eaee869012acb74e1
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-03-15 19:20:39 -0300

    Adicionando comentário na questão 09

--- Lista de exercícios 01/09.c
+++ Lista de exercícios 01/09.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+/*
+    9.	Um motorista de táxi deseja calcular o rendimento de seu carro na praça.
+        Sabendo-se que o preço do combustível é de R$ 1,90, escreva um programa
+        para ler: a marcação do odômetro (Km) no início do dia, a marcação (Km)
+        no final do dia, o número de litros de combustível gasto e o valor total
+        (R$) recebido dos passageiros. Calcular e escrever: a média do consumo
+        em Km/L e o lucro (líquido) do dia.
+*/
 int main(){
     float preco_combustivel = 1.9;
     float km_inicio, km_fim, km_rodados, combustivel_gasto, total_recebido, media_consumo, lucro, custo;
-
     printf("Informe a marcacao do odometro no inicio do dia: ");
     scanf("%f", &km_inicio);
     printf("Informe a marcacao do odometro no final do dia: ");
