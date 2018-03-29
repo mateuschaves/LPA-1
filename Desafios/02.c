ab80a3e9f759881f5c8357fdde46f8507a63c561
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-03-28 21:58:39 -0300

    Desafio 02

--- Desafios/02.c
+++ Desafios/02.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+/*
+	Tem-se um conjunto de dados contendo a altura 25 pessoas. Faça um programa que calcule e mostre: 
+	*	a maior altura;
+	*	menor altura; 
+	*	a média de altura;
+*/
 int main(void){
 	int altura, maior, menor, i, c = 0;
 	float media;
-	for(i = 0; i < 3; i++){
+	for(i = 0; i < 25; i++){
 		printf("Qual a altura ? ");
 		scanf("%d", &altura);
 		if(c == 0){
