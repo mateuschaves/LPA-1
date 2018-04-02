d66633d6273b690f3ab791cb336cf1062f7a6a05
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-04-02 02:19:22 -0300

    Adicionando comentários a questão 04

--- Lista de exercícios 01/04.c
+++ Lista de exercícios 01/04.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
     4.	Escreva um programa para ler uma temperatura em graus Fahrenheit,
         calcular e escrever o valor correspondente em graus Celsius.
 */
 int main(){
+    // Declarando as variáveis
     float celsius, fahrenheit;
     printf("Informe a temperatura em graus celsisu : ");
+    // Lendo o valor do usuário
     scanf("%f", &celsius);
+    // Calculando a temperatura em fahrenheit
     fahrenheit = ((celsius / 5)*9) + 32;
-    printf("A temperatura em fahrenheit eh de : %.2f", fahrenheit);
-    printf("\nPressione qualquer tecla para sair do programa...");
-    scanf("%f", &celsius);
+    // Mostrando o valor na tela
+    printf("A temperatura em fahrenheit eh de : %.2f\n", fahrenheit);
+    system("pause");
 }
