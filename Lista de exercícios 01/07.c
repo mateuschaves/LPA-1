b85ac6b719b8f9ec90e5b9a4530eafbd0e063569
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-04-02 02:32:35 -0300

    Adicionando comentários na questão 07

--- Lista de exercícios 01/07.c
+++ Lista de exercícios 01/07.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 /*
 7.	Escreva um programa para calcular e imprimir o número de lâmpadas
     necessárias para iluminar um determinado cômodo de uma residência. 
@@ -9,22 +9,27 @@
     Considere que a potência necessária é de 18 watts por metro quadrado.
 */
 int main(){
+    // Declarando as variáveis
     float largura, comprimento, area, potencia;
     int potencia_metro_quadrado = 18;
     int lampadas;
-
     printf("Potencia da lampada utilizada: ");
+    // Lendo o valor do usuário
     scanf("%f", &potencia);
     printf("Largura do comodo: ");
+    // Lendo o valor do usuário
     scanf("%f", &largura);
     printf("Comprimento do comodo: ");
+    // Lendo o valor do usuário
     scanf("%f", &comprimento);
+    // Calculando a área
     area = largura*comprimento;
+    // Calculando quantas lâmpadas são necessárias
     lampadas = ceil((area*potencia_metro_quadrado) / potencia);
     if(lampadas > 1){
-        printf("Voce precisa de %i lampadas", lampadas);
+        printf("Voce precisa de %i lampadas\n", lampadas);
     }else{
-        printf("você precisa de %i lampada", lampadas);
+        printf("você precisa de %i lampada\n", lampadas);
     }
-    scanf("%i", &lampadas);
+    system("pause");
 }
