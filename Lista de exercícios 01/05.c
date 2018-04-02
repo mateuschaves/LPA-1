90f73e0b5e419434a371d9d16974912beb98986a
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-04-02 02:22:27 -0300

    Adicionando comentários a questão 05

--- Lista de exercícios 01/05.c
+++ Lista de exercícios 01/05.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
     5.	Calcular o consumo médio do veículo, sendo informados a distância percorrida(D)
         e o número de litros consumidos (LIT). Formula: (CM = D/LIT).
 */
 int main(){
+    // Declarando as variáveis
     float distancia, litros, media;
     printf("Informe a distancia percorrida : ");
+    // Lendo o valor do usuário
     scanf("%f", &distancia);
     printf("Informe a quantidade de litros consumidos: ");
+    // Lendo o valor do usuário
     scanf("%f", &litros);
+    // Calculando o consumo médio do veículo
     media = distancia/litros;
-    printf("O consumo medio do veiculo eh de : %.2f", media);
-    printf("\nPressione qualquer tecla para sair do programa.");
-    scanf("%f", &litros);
+    // Mostrando a media de consumo do veículo
+    printf("O consumo medio do veiculo eh de : %.2f\n", media);
+    system("pause");
 }
