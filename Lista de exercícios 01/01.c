4462772076eb4040ff181a865c9a39c9d8b430d1
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-04-02 02:06:17 -0300

    Comentários

--- Lista de exercícios 01/01.c
+++ Lista de exercícios 01/01.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 /*
  * 1. Escreva um programa para ler o raio de um círculo, calcular e escrever a sua área.
  */
-
 int main(){
+    // Declarando a variável
     float r;
     printf("Digite o raio do circulo: ");
+    // Lendo a variável do usuário
     scanf("%f", &r);
-    printf("\nA area do circulo eh de : %.2f", pow(r, 2) * 3.1415926535898);
-    printf("\nPressione qualquer tecla para sair do programa.");
-    scanf("%f", r);
+    // Mostrando o resultado
+    printf("\nA area do circulo eh de : %.2f\n", pow(r, 2) * 3.1415926535898);
+    system("pause");
 }
