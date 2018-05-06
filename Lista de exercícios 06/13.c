16f0b8304c52fd59972fe7092e8b99c1e88de304
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-05-06 04:08:51 -0300

    enunciado da questão

--- Lista de exercícios 06/13.c
+++ Lista de exercícios 06/13.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*
+    13.	Em uma competição de salto em distância cada atleta tem direito a cinco saltos.
+        O resultado do atleta será determinado pela média dos cinco valores restantes.
+        Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas
+        pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos.
+        O programa deve ser encerrado quando não for informado o nome do atleta
+*/
 void main(void){
     float saltos[5], media;
     int i, parada = 0, tamanho = sizeof(saltos)/sizeof(int);
