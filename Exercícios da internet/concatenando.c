37189d40b2c8b5b490e35d26016be68cb28c080e
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-05-08 00:40:25 -0300

    Update concatenando.c

--- Exercícios da internet/concatenando.c
+++ Exercícios da internet/concatenando.c
@@ -7,21 +7,21 @@ int main(void) {
   char novaPalavra[13];
   int i, o, c = 0, p = 0;
   for(i = 0; i < 13; i++){
-		if(i < 4){
-			novaPalavra[i] = palavra1[i];
-		}else{
-			for(o = 5; o <= 15; o++){
-				novaPalavra[o] = palavra2[p];
-				p++;
-			}
-			c++;
-		}
-		if(c > 0){
-			break;
-		}
+  	if(i < 4){
+	     novaPalavra[i] = palavra1[i];
+	}else{
+	     for(o = 5; o <= 15; o++){
+	     	novaPalavra[o] = palavra2[p];
+		p++;
+	      }
+	c++;
+	}
+	if(c > 0){
+	    break;
+	}
   }
   for(i = 0; i < 14; i++){
-  		printf("%c", novaPalavra[i]);
+  	printf("%c", novaPalavra[i]);
   }
   printf("\n");
   system("pause");
