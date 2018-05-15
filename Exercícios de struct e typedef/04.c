0ac4bf619fe4f2a8f88cc5e65744957bad354b7c
MATEUS HENRIQUE CHAVES ARAUJO MORAIS <11505802407@UPE.IO>
2018-05-15 09:28:19 -0300

    loop =)

--- Exercícios de struct e typedef/04.c
+++ Exercícios de struct e typedef/04.c
@@ -7,21 +7,23 @@ void main(void){
         char nome[20];
         float n1, n2, med;
     } alunos;
+    int i;
+    alunos alunoss[10];
+    for(i = 0; i < 20; i++){
+        printf("Informe a matricula do aluno: ");
+        scanf("%i", &alunoss[i].mat);
+        printf("Informe a primeira nota: ");
+        scanf("%f", &alunoss[i].n1);
+        printf("Informe a segunda nota: ");
+        scanf("%f", &alunoss[i].n2);
+        printf("Informe o nome do aluno: ");
+        fflush(stdin);
+        gets(alunoss[i].nome);
+        alunoss[i].med = (alunoss[i].n1 + alunoss[i].n2) / 2;
+        printf("A media do aluno %s eh : %f", alunoss[i].nome, alunoss[i].med);
+        printf("\nMatricula: %i\n", alunoss[i].mat);
 
-    alunos aluno;
-
-    printf("Informe a matricula do aluno: ");
-    scanf("%i", &aluno.mat);
-    printf("Informe a primeira nota: ");
-    scanf("%f", &aluno.n1);
-    printf("Informe a segunda nota: ");
-    scanf("%f", &aluno.n2);
-    printf("Informe o nome do aluno: ");
-    fflush(stdin);
-    gets(aluno.nome);
-    aluno.med = (aluno.n1 + aluno.n2) / 2;
-    printf("A media do aluno %s eh : %f", aluno.nome, aluno.med);
-    printf("\nMatricula: %i\n", aluno.mat);
-
+    }
+    
     system("pause");
 }
