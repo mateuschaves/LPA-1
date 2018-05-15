64f34c6cf4d9bc5e24fedd468aa6e70624a565c7
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-05-15 00:15:56 -0300

    explicando o código

--- Exercícios de struct e typedef/typedef.c
+++ Exercícios de struct e typedef/typedef.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 
 void main(void){
+    // Cria uma estrutura ' classe ' chamada dma.
     struct dma{
+        // Propriedades da ' classe ' dma.
         int dia;
         int mes;
         int ano;
     };
+    // Renomeando o tipo ' struct dma ' para ' data '.
     typedef struct dma data;
+    // Instânciando a ' classe ' dma que agora é ' data '. 
     data hoje;
+    // Acessando as propriedades da ' classe '.
     hoje.dia = 14;
     hoje.mes = 5;
     hoje.ano = 2018;
