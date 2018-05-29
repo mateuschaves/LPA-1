fac7ae7765ddc7373b7f2b5c95e663afb16d60e9
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-05-29 01:41:41 -0300

    Fibonacci function

---
+++ Exercícios da internet/fibonacci_function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n);
+
+void main(void){
+    int n, valor;
+    printf("Informe a posicao da sequencia: ");
+    scanf("%i", &n);
+    valor = fibonacci(n);
+    printf("Valor: %i\n\n", valor);
+    system("pause");
+}
+
+int fibonacci(int n){
+    int i, squence[n];
+    squence[0] = 0;
+    squence[1] = 1;
+    for(i = 2; i < n; i++){
+        squence[i] = squence[i - 1] + squence[i - 2];
+    }
+    return squence[i - 1];
+}
