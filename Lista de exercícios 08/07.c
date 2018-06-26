f79bc9a8ad3e7c0deed07f84b00cfabcb9733e36
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-06-25 22:20:29 -0300

    comentários

--- Lista de exercícios 08/07.c
+++ Lista de exercícios 08/07.c
@@ -29,40 +29,56 @@ void main(void){
         if(i + 1 > 1){
             printf("\n\n");
         }
+        // MENU
         printf("Choose a option:\n1 - Register\n2 - Update\n3 - Print\n4 - Exit\n >>>>   ");
         scanf("%i", &menu);
+        // Testando a opção escolhida no menu.
         switch(menu){
+                // Caso for digitado 1, fazer o cadastro de um novo cliente.
                 case 1:
+                    // Nome do cliente.
                     printf("Type client's name: ");
                     fflush(stdin);
                     gets(client[i].name);
+                    // Código do cliente.
                     printf("Type client's code: ");
                     scanf("%i", &client[i].code);
+                    // Idade do cliente.
                     printf("Type age's client: ");
                     fflush(stdin);
                     scanf("%i", &client[i].age);
+                    // Cpf do cliente.
                     printf("Type cpf's client: ");
                     fflush(stdin);
                     scanf("%i", &client[i].cpf);
+                    // Endereço do cliente.
                     printf("Type address's client: ");
                     fflush(stdin);
                     gets(client[i].address);
+                    // Sexo do cliente.
                     printf("Type gender's client: ");
                     fflush(stdin);
                     scanf("%c", &client[i].gender);
+                    // i++ serve para somar mais 1 na variável que conta a quantidade de clientes cadastrados.
                     i++;
                     break;
+                // Caso for digitado 2, irá alterar as informções de um cliente já cadastrado.
                 case 2:
+                    // Lendo o código do cliente que deseja atualizar.
                     printf("Type client's ID: ");
                     scanf("%i", &code);
+                    // Procurando no vetor de clientes o cliente com o código informado.
                     for(int o = 0; o < i; o++){
+                        // Verificando se o código informado é igual o do cliente no índice o;
                         if(client[o].code == code){
                             check++;
+                            // Se sim, o índice 'o' é salvo da variável índice.
                             index = o;
                             break;
                         }
                     }
                     if(check > 0 ){
+                        // Pedindo as novas informações do cliente.
                         printf("Type the new client's name: ");
                         fflush(stdin);
                         gets(client[index].name);
@@ -84,22 +100,31 @@ void main(void){
                     check = 0;
                     index = 0;
                     break;
+                // Caso for digitado 3, irá mostrar as informções de um cliente já cadastrado.
                 case 3:
+                    // Pedindo o código do cliente.
                     printf("Type the client's code: ");
                     scanf("%i", &code);
+                    // Procurando no vetor de clientes o cliente com o código informado.
                     for(int o = 0; o < i; o++){
+                        // Verificando se o código informado é igual o do cliente no índice o;
                         if(code == client[o].code){
                             check++;
+                            // Se sim, o índice 'o' é salvo da variável índice.
                             index = o;
                             break;
                         }
                     }
+                    // Mostrando as informações do cliente.
+                    // A variável index vai guardar o índice do cliente com o código informado.
                     printf("\n\nName: %s\n", client[index].name);
                     printf("Age: %i\n", client[index].age);
                     printf("Address: %s\n", client[index].address);
                     printf("Gender: %c\n", client[index].gender);
                     printf("Code: %i\n", client[index].code);
+                // Caso for digitado 4, O loop se encerra.
                 case 4:
+                    // Quando parada for igual a 1, o loop encerra.
                     parada = 1;
                     break;
                 default:
