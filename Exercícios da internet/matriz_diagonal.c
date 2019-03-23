53c3bff6d3d15247ba098758e517541399abe362
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-03-23 13:58:02 -0300

    Problem has been solved: waiting for an optimization

--- Exercícios da internet/matriz_diagonal.c
+++ Exercícios da internet/matriz_diagonal.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdbool.h> 
+#include <time.h>   
+
+#define RED "\x1b[31m"
+#define COLOR_RESET "\x1b[0m"
 
 int isDiagonalTopRight_ToBottomLeft_Ok(int line, int column);
 int isDiagonalTopLeft_ToBottomRight_Ok(int line, int column);
@@ -8,9 +12,25 @@ int isDiagonalTopLeft_ToBottomRight_Ok(int line, int column);
 int isHorizontalOk(int column);
 int isVerticalOk(int line);
 
+typedef struct T_positions{
+    int line;
+    int column;
+}T_positions;
+
 void main(void){
     int greatestProductOfThreeNumbers = 0;
+    int position1, position2, position3, position4;
+    const int RANGE = 10;
+    srand( (unsigned)time(NULL) ); 
+    T_positions pos[4];
 
+    system("cls");
+
+    int matrix[20][20];
+
+    // Uncomment this if you want to put into the array numbers manually
+
+    /*
     int matrix[20][20] = {
         4, 0, 2, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
         0, 5, 0, 5, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0,
@@ -33,37 +53,111 @@ void main(void){
         0, 9, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         6, 5, 1, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     };
+    */
+
+    // Create a 20x20 array with random numbers
+    for(int x = 0; x <= 19; x++){
+        for(int y = 0; y <= 19; y++){
+            matrix[x][y] = rand() % RANGE;
+        }
+    }
 
     for(int line = 0; line <= 19; line++){
         for(int column = 0; column <= 19; column++){
 
             if(isDiagonalTopRight_ToBottomLeft_Ok(line, column)){
-                if((matrix[line][column] * matrix[line + 1][column - 1] * matrix[line + 2][column - 2] * matrix[line + 3][column - 3]) > greatestProductOfThreeNumbers){   // Diagonal Top Right to Bottom Left
+                if((matrix[line][column] * matrix[line + 1][column - 1] * matrix[line + 2][column - 2] * matrix[line + 3][column - 3]) > greatestProductOfThreeNumbers){
                     greatestProductOfThreeNumbers = matrix[line][column] * matrix[line + 1][column - 1] * matrix[line + 2][column - 2] * matrix[line + 3][column - 3];
+
+                    pos[0].line = line;
+                    pos[0].column = column;
+
+                    pos[1].line = line + 1;
+                    pos[1].column = column - 1;
+                    
+                    pos[2].line = line + 2;
+                    pos[2].column = column - 2;
+                    
+                    pos[3].line = line + 3;
+                    pos[3].column = column - 3;
+                                        
                 }
             }
 
             if(isDiagonalTopLeft_ToBottomRight_Ok(line, column)){
-                if((matrix[line][column] * matrix[line + 1][column + 1] * matrix[line + 2][column + 2] * matrix[line + 3][column + 3]) > greatestProductOfThreeNumbers){   //  Diagonal Top Left to top Right
+                if((matrix[line][column] * matrix[line + 1][column + 1] * matrix[line + 2][column + 2] * matrix[line + 3][column + 3]) > greatestProductOfThreeNumbers){
                     greatestProductOfThreeNumbers = matrix[line][column] * matrix[line + 1][column + 1] * matrix[line + 2][column + 2] * matrix[line + 3][column + 3];
+                    
+                    pos[0].line = line;
+                    pos[0].column = column;
+
+                    pos[1].line = line + 1;
+                    pos[1].column = column + 1;
+                    
+                    pos[2].line = line + 2;
+                    pos[2].column = column + 2;
+                    
+                    pos[3].line = line + 3;
+                    pos[3].column = column + 3;
                 }
             }
 
             if(isHorizontalOk(column)){
-                if((matrix[line][column] * matrix[line][column + 1] * matrix[line][column + 2] * matrix[line][column + 3]) > greatestProductOfThreeNumbers){               // Horizontal
+                if((matrix[line][column] * matrix[line][column + 1] * matrix[line][column + 2] * matrix[line][column + 3]) > greatestProductOfThreeNumbers){
                     greatestProductOfThreeNumbers = matrix[line][column] * matrix[line][column + 1] * matrix[line][column + 2] * matrix[line][column + 3];
+
+                    pos[0].line = line;
+                    pos[0].column = column;
+
+                    pos[1].line = line;
+                    pos[1].column = column + 1;
+                    
+                    pos[2].line = line;
+                    pos[2].column = column + 2;
+                    
+                    pos[3].line = line;
+                    pos[3].column = column + 3;
                 }
             }
             if(isVerticalOk(line)){
-                if((matrix[line][column] * matrix[line + 1][column] * matrix[line + 2][column] * matrix[line + 3][column]) > greatestProductOfThreeNumbers){               // Vertical
+                if((matrix[line][column] * matrix[line + 1][column] * matrix[line + 2][column] * matrix[line + 3][column]) > greatestProductOfThreeNumbers){
                     greatestProductOfThreeNumbers = matrix[line][column] * matrix[line + 1][column] * matrix[line + 2][column] * matrix[line + 3][column];
+
+                    pos[0].line = line;
+                    pos[0].column = column;
+
+                    pos[1].line = line + 1;
+                    pos[1].column = column;
+                    
+                    pos[2].line = line + 2;
+                    pos[2].column = column;
+                    
+                    pos[3].line = line + 3;
+                    pos[3].column = column;
                 }
             }
 
         }
     }
- 
-    printf("\n\n Greatest Product of four numbers = %d\n\n", greatestProductOfThreeNumbers);
+    
+
+    // Output
+
+    printf("\n\n");
+
+    for(int x = 0; x <= 19; x++){
+        for(int y = 0; y <= 19; y++){
+            if((x == pos[0].line && y == pos[0].column) || (x == pos[1].line && y == pos[1].column) || (x == pos[2].line &&  y == pos[2].column) || (x == pos[3].line  && y == pos[3].column)){
+                printf(RED " %d, " COLOR_RESET, matrix[x][y]);
+            }else{
+                printf(" %d, ", matrix[x][y]);
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n\n Greatest Product of four numbers: ");
+    printf(" %d * %d * %d * %d = %d\n\n\n", matrix[pos[0].line][pos[0].column],  matrix[pos[1].line][pos[1].column], matrix[pos[2].line][pos[2].column], matrix[pos[3].line][pos[3].column], greatestProductOfThreeNumbers);
 
     system("pause");
 }
